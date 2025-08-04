b4b678bf3e8bb2454f6e5ea77351dd5b276e76db
Narayana Chebolu <narayana.chebolu@gmail.com>
2025-08-05 00:11:10 +0200

    updated to string reverse.

--- src/main/java/org/com/core/basic/StringReverse.java
+++ src/main/java/org/com/core/basic/StringReverse.java
@@ -1,12 +1,32 @@
 package org.com.core.basic;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class StringReverse {
     public static void main(String[] args) {
         String str = "HowAreYou?";
+
+        System.out.println(conventionalWay(str));
+
+        System.out.println("Using java streams: " + strReverse(str));
+    }
+
+    private static String conventionalWay(String str) {
         StringBuilder rev = new StringBuilder();
         for (int i = str.length() - 1; i >= 0; i--) {
             rev.append(str.charAt(i));
         }
-        System.out.println(rev);
+
+        return rev.toString();
+    }
+
+    private static String strReverse(String str) {
+        StringBuilder rev = new StringBuilder();
+        return IntStream
+                .rangeClosed(1, str.length())
+                .mapToObj(i -> str.charAt(str.length() - i))
+                .map(String::valueOf)
+                .collect(Collectors.joining());
     }
 }
