8fe23a2a39e28458b33724ed5ff677fcabd34eb4
Narayana Chebolu <narayana.chebolu@gmail.com>
2025-08-06 23:28:47 +0200

    Count by vowels.

--- src/main/java/org/com/core/basic/CountVowels.java
+++ src/main/java/org/com/core/basic/CountVowels.java
@@ -3,12 +3,16 @@ package org.com.core.basic;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class CountVowels {
     public static void main(String[] args) {
-        String str = "Count vowels in a given string.";
+        String str = "Functional Programming with Java Streams";
         System.out.println("Vowels in a given string are " + countVowels(str));
+
+        System.out.println("Count by each vowel: " + countByVowels(str));
     }
 
     private static long countVowels(String str) {
@@ -18,9 +22,21 @@ public class CountVowels {
 
         Set<Character> vowels = Set.of('a', 'e', 'i', 'o', 'u');
 
-        return str.toLowerCase().chars()
+        return str.toLowerCase().chars() // returns Intstream of chars
+                .mapToObj(c -> (char)c) // convert integer to char
+                .filter(vowels::contains) // keep only vowels
+                .count(); // count vowels
+    }
+
+    private static Map<Character, Long> countByVowels(String str) {
+        if (str == null || str.isEmpty()) {
+            return new HashMap<>();
+        }
+
+        return str.toLowerCase()
+                .chars()
                 .mapToObj(c -> (char)c)
-                .filter(vowels::contains)
-                .count();
+                .filter(c -> "aeiou".indexOf(c) >= 0)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
     }
 }
